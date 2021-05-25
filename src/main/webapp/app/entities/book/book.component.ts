aaadd8cf3b42173dfa5a51c8445004529baec16c
Damien Ji ZiYang <ziyang.ji@ncsi.com.cn>
2021-05-25 17:54:39 +0800

    add search condition

--- src/main/webapp/app/entities/book/book.component.ts
+++ src/main/webapp/app/entities/book/book.component.ts
@@ -9,10 +9,12 @@ import { IBook } from 'app/shared/model/book.model';
 import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
 import { BookService } from './book.service';
 import { BookDeleteDialogComponent } from './book-delete-dialog.component';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'ehc-book',
   templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit, OnDestroy {
   books: IBook[];
@@ -22,14 +24,24 @@ export class BookComponent implements OnInit, OnDestroy {
   page: number;
   predicate: string;
   ascending: boolean;
+  search: any;
+
+  searchForm = this.fb.group({
+    name: [],
+    position: [],
+    author: [],
+    detail: [],
+  });
 
   constructor(
     protected bookService: BookService,
     protected eventManager: JhiEventManager,
     protected modalService: NgbModal,
-    protected parseLinks: JhiParseLinks
+    protected parseLinks: JhiParseLinks,
+    protected fb: FormBuilder
   ) {
     this.books = [];
+    this.search = {};
     this.itemsPerPage = ITEMS_PER_PAGE;
     this.page = 0;
     this.links = {
@@ -39,14 +51,29 @@ export class BookComponent implements OnInit, OnDestroy {
     this.ascending = true;
   }
 
+  resetCondition(): void {
+    this.searchForm.reset();
+    this.search = {};
+    this.reset();
+  }
+
+  searching(): void {
+    this.search = Object.assign({}, this.searchForm.value);
+    this.search.position = this.search.position?.code;
+    Object.keys(this.search).forEach(key => {
+      if (this.search[key]) {
+        this.search[key + '.contains'] = this.search[key];
+      }
+      Reflect.deleteProperty(this.search, key);
+    });
+    this.reset();
+  }
+
   loadAll(): void {
-    this.bookService
-      .query({
-        page: this.page,
-        size: this.itemsPerPage,
-        sort: this.sort(),
-      })
-      .subscribe((res: HttpResponse<IBook[]>) => this.paginateBooks(res.body, res.headers));
+    this.search['page'] = this.page;
+    this.search['size'] = this.itemsPerPage;
+    this.search['sort'] = this.sort();
+    this.bookService.query(this.search).subscribe((res: HttpResponse<IBook[]>) => this.paginateBooks(res.body, res.headers));
   }
 
   reset(): void {
--- src/main/webapp/app/entities/book/book.module.ts
+++ src/main/webapp/app/entities/book/book.module.ts
@@ -7,9 +7,12 @@ import { BookDetailComponent } from './book-detail.component';
 import { BookUpdateComponent } from './book-update.component';
 import { BookDeleteDialogComponent } from './book-delete-dialog.component';
 import { bookRoute } from './book.route';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  imports: [EhcMonolithSampleSharedModule, RouterModule.forChild(bookRoute)],
+  imports: [EhcMonolithSampleSharedModule, CardModule, ButtonModule, InputTextModule, RouterModule.forChild(bookRoute)],
   declarations: [BookComponent, BookDetailComponent, BookUpdateComponent, BookDeleteDialogComponent],
   entryComponents: [BookDeleteDialogComponent],
 })
